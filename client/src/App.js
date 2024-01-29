7a0f31a9b1ca099f3a1fc0de4d87c0c724a9fc43
Gagana97 <cgagana97@gmail.com>
2024-01-29 14:47:20 +0530

    firstSave

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Signup from './Pages/Signup';
+
+
+function App() {
+  return (
+    <div className="text-red-500 p-2">
+  <Signup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Signup.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Signup</h2>
+  </div>
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" action="#" method="POST">
+      <div>
+      <label for="Fname" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+      <div class="mt-2">
+          <input id="Fname" name="Fname" type="Fname" autocomplete="Fname" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+        <label for="Lname" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
+      <div class="mt-2">
+          <input id="Lname" name="Lname" type="Lname" autocomplete="Lname" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+        <label for="Phonenumber" class="block text-sm font-medium leading-6 text-gray-900">Phone number</label>
+      <div class="mt-2">
+          <input id="Phonenumber" name="Phonenumber" type="Phonenumber" autocomplete="Phonenumber" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div class="mt-2">
+          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+          
+        </div>
+        <div class="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+      <label for="Re-enter password" class="block text-sm font-medium leading-6 text-gray-900">Re-enter Password</label>
+      <div class="mt-2">
+          <input id="Re-enter password" name="Re-enter password" type="Re-enter password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      </div>
+    </form>
+    <p class="mt-10 text-center text-sm text-gray-500">
+      Not a member?
+      <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+    </p>
+  </div>
+</div>
+
+
+
+
+
+
+
+  )
+}
+
+export default Signup
