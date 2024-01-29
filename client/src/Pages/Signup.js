d266a4e2df022c44bfeff28f902f157e05cf00bc
Gagana97 <cgagana97@gmail.com>
2024-01-29 17:52:33 +0530

    commit

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,7 +1,54 @@
 import React from 'react'
+import { useState } from "react";
 
 
 const Signup = () => {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [PhoneNumber, setphoneNumber] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState({});
+
+  const checkValidation = (e) => {
+    const newError = {};
+    if (e.target.firstName.value === "") {
+      newError.firstname = "Enter your First Name";
+    }
+
+    if (e.target.lastName.value === "") {
+      newError.lastName = "Enter your Last Name";
+    }
+
+    if (e.target.PhoneNumber.value === "") {
+      newError.contact = "Enter your Contact Number";
+    }
+
+    if (e.target.email.value === "") {
+      newError.email = "Enter your Email";
+    }
+
+    if (e.target.password.value === "") {
+      newError.password = "Enter your Password";
+    }
+
+   
+    setError(newError);
+    return newError;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const firstName = e.target.firstName.value;
+    const lastName = e.target.lastName.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const PhoneNumber = e.target.PhoneNumber.value;
+
+    
+          }
+    
+  
   return (
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -9,36 +56,48 @@ const Signup = () => {
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Signup</h2>
   </div>
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+  <form onSubmit={(e) => handleSubmit(e)} className='bg-white shadow-md rounded px-10 pb-8 mb-4'>
       <div>
-      <label for="Fname" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
+      <label for="firstName" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
       <div class="mt-2">
-          <input id="Fname" name="Fname" type="Fname" autocomplete="Fname" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-        </div>
+      <input id='firstName' type="text" name="firstName" value={firstName} className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+      {error.firstname && <span className='valid'>{error.firstname}</span>} </div>
         <label for="Lname" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
       <div class="mt-2">
-          <input id="Lname" name="Lname" type="Lname" autocomplete="Lname" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+      <input id="lastName" name="lastName" type="lastName" autoComplete="off" value={lastName}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                      {error.lastName && <span className='valid'>{error.lastName}</span>}
         </div>
-        <label for="Phonenumber" class="block text-sm font-medium leading-6 text-gray-900">Phone number</label>
+        <label for="PhoneNumber" class="block text-sm font-medium leading-6 text-gray-900">Phone number</label>
       <div class="mt-2">
-          <input id="Phonenumber" name="Phonenumber" type="Phonenumber" autocomplete="Phonenumber" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+      <input id="PhoneNumber" name="PhoneNumber" type="PhoneNumber" autoComplete="off" value={PhoneNumber}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                      {error.PhoneNumber && <span className='valid'>{error.PhoneNumber}</span>}
         </div>
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
         <div class="mt-2">
-          <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        <input id="email" name="email" type="email" autoComplete="off" value={email}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                      {error.email && <span className='valid'>{error.email}</span>}
         </div>
      <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          <input id="password" name="password" type="password" autoComplete="off" value={password}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                      {error.password && <span className='valid'>{error.password}</span>}
         </div>
       
       <label for="Confirm password" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
       <div class="mt-2">
-          <input id="Confirm password" name="Confirm password" type="Confirm password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+      <input id="password" name="password" type="password" autoComplete="off" value={password}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                      {error.password && <span className='valid'>{error.password}</span>}
         </div>
         </div>
       <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Signup</button>
+      <button type='Submit'  onSubmit={(e) => handleSubmit(e)} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-bright-orange hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Sign up
+                    </button>
       </div>
       
     </form>
