fd5b215f4f5a54c69aa76212786877475a171b2d
DineshLogan <dineshlogan31@gmail.com>
2024-01-29 16:25:20 +0530

    getUserList

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,12 @@
 const express=require("express");
-const { getUser, SignupUser } = require("../controller/userController");
+const { getUser, SignupUser, getSignUpUserList } = require("../controller/userController");
 
 const userRouter=express.Router();
 
 userRouter.get("/user",getUser);
 
 userRouter.post("/user/signUpUser",SignupUser)
+
+userRouter.get("/user/getSignUpUserList",getSignUpUserList)
 
 module.exports=userRouter;
